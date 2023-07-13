13afd177a8bec59a5bbca3252438c62192c9964e
Кшнякин Александр Владимирович <avkshniakin@domclick.ru>
2023-07-14 00:38:19 +0300

    fix minmum default level of test

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2023_07_11_210812) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", default: 1, null: false
+    t.integer "level", default: 0, null: false
     t.integer "category_id", null: false
     t.integer "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
