a34c0f4866f88a7d8d9def437cd5e32f69c03033
Кшнякин Александр Владимирович <avkshniakin@domclick.ru>
2023-07-16 13:06:19 +0300

    fix review comments

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,8 @@
 
 class Test < ApplicationRecord
   def self.list_by_category(category_title)
-    Test.joins('INNER JOIN categories ON tests.category_id = categories.id')
-        .where(categories: { title: category_title }).order(:title)
+    joins('INNER JOIN categories ON tests.category_id = categories.id')
+      .where(categories: { title: category_title })
+      .order(title: :desc)
   end
 end
