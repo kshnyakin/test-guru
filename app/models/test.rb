e582ff1233fc5cc9f261f7c94a3a7ff440704f31
Кшнякин Александр Владимирович <avkshniakin@domclick.ru>
2023-07-17 09:08:06 +0300

    fix onle titles

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,5 +5,6 @@ class Test < ApplicationRecord
     joins('INNER JOIN categories ON tests.category_id = categories.id')
       .where(categories: { title: category_title })
       .order(title: :desc)
+      .pluck(:title)
   end
 end
