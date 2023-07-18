aff8d0b0d83046b6c134ae5d727de8a7eeef2039
Кшнякин Александр Владимирович <avkshniakin@domclick.ru>
2023-07-18 22:50:05 +0300

    works at associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,10 @@
 
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :questions
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :test_passings
+  has_many :users, through: :test_passings
 
   def self.list_by_category(category_title)
     joins('INNER JOIN categories ON tests.category_id = categories.id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
+  has_many :test_passings
+  has_many :tests, through: :test_passings
+
   def tests_by_level(level)
     Test.joins('INNER JOIN test_passings ON tests.id = test_passings.test_id')
         .where(level: level, test_passings: { user_id: id })
