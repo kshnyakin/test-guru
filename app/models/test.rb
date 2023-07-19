f58dc6139a0afbc8ffc8994830032a7b43e74332
Кшнякин Александр Владимирович <avkshniakin@domclick.ru>
2023-07-20 00:23:41 +0300

    fix review comments;

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  belongs_to :author, class_name: 'User'
   belongs_to :category
-  has_many :questions
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :test_passings
+
+  has_many :questions, dependent: :destroy
+  has_many :test_passings, dependent: :destroy
   has_many :users, through: :test_passings
 
   def self.list_by_category(category_title)
-    joins('INNER JOIN categories ON tests.category_id = categories.id')
+    joins(:category)
       .where(categories: { title: category_title })
       .order(title: :desc)
       .pluck(:title)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 # Создание категорий
 backend_category, frontend_category, devops_category, management_category, _hr_category =
   Category.create!([
@@ -27,59 +19,89 @@ ivanov_user, petrov_user, sidorov_user =
                ])
 
 # Создание тестов
-ruby_test, java_test, angular_test, bulma_test, kubernetis_test, gitlab_test, jira_test =
-  Test.create!([
-                 { title: 'Ruby', level: 1, category_id: backend_category.id, author_id: ivanov_user.id },
-                 { title: 'Java', level: 2, category_id: backend_category.id, author_id: petrov_user.id },
-                 { title: 'Angular', level: 3, category_id: frontend_category.id, author_id: sidorov_user.id },
-                 { title: 'Bulma', level: 1, category_id: frontend_category.id, author_id: ivanov_user.id },
-                 { title: 'Kubernetis', level: 2, category_id: devops_category.id, author_id: petrov_user.id },
-                 { title: 'Gitlab', level: 3, category_id: devops_category.id, author_id: sidorov_user.id },
-                 { title: 'Jira', level: 4, category_id: management_category.id, author_id: ivanov_user.id }
-               ])
+ruby_test, java_test =
+  backend_category.tests.create!([
+                                   { title: 'Ruby', level: 1, author_id: ivanov_user.id },
+                                   { title: 'Java', level: 2, author_id: petrov_user.id }
+                                 ])
+angular_test, bulma_test =
+  frontend_category.tests.create!([
+                                    { title: 'Angular', level: 3, author_id: sidorov_user.id },
+                                    { title: 'Bulma', level: 1, author_id: ivanov_user.id }
+                                  ])
+kubernetis_test, gitlab_test =
+  devops_category.tests.create!([
+                                  { title: 'Kubernetis', level: 2,
+                                    author_id: petrov_user.id },
+                                  { title: 'Gitlab', level: 3,
+                                    author_id: sidorov_user.id }
+                                ])
+jira_test = management_category.tests.create!(
+  { title: 'Jira', level: 4, author_id: ivanov_user.id }
+)
 
 # Создание вопросов
-q_ruby_data_types, q_ruby_class_define, q_jira_my_tasks, q_jira_team_tasks, q_kuber_pods_quantity, q_kuber_pod_exec =
-  Question.create!([
-                     { title: 'Типы данных в Ruby', body: 'Какой из перечисленных вариантов верный?',
-                       test_id: ruby_test.id },
-                     { title: 'Определение класса', body: 'Какой из перечисленных вариантов верный?',
-                       test_id: ruby_test.id },
-                     { title: 'Как посмотреть список своих задач?', body: 'Какой из перечисленных вариантов верный?',
-                       test_id: jira_test.id },
-                     { title: 'Как посмотреть список задач команды?', body: 'Какой из перечисленных вариантов верный?',
-                       test_id: jira_test.id },
-                     { title: 'Как увеличить количество подов?', body: 'Какой из перечисленных вариантов верный?',
-                       test_id: kubernetis_test.id },
-                     { title: 'Как войти в консоль нужного пода?', body: 'Какой из перечисленных вариантов верный?',
-                       test_id: kubernetis_test.id }
-                   ])
+q_ruby_data_types, q_ruby_class_define =
+  ruby_test.questions.create!([
+                                { title: 'Типы данных в Ruby',
+                                  body: 'Какой из перечисленных вариантов верный?' },
+                                { title: 'Определение класса',
+                                  body: 'Какой из перечисленных вариантов верный?' }
+                              ])
+q_jira_my_tasks, _q_jira_team_tasks =
+  jira_test.questions.create!([
+                                { title: 'Как посмотреть список своих задач?',
+                                  body: 'Какой из перечисленных вариантов верный?' },
+                                { title: 'Как посмотреть список задач команды?',
+                                  body: 'Какой из перечисленных вариантов верный?' }
+                              ])
+q_kuber_pods_quantity, q_kuber_pod_exec =
+  kubernetis_test.questions.create!([
+                                      {
+                                        title: 'Как увеличить количество подов?',
+                                        body: 'Какой из перечисленных вариантов верный?'
+                                      },
+                                      {
+                                        title: 'Как войти в консоль нужного пода?',
+                                        body: 'Какой из перечисленных вариантов верный?'
+                                      }
+                                    ])
 
 # Создание ответов
-Answer.create!([
-                 { body: 'String, Integer, Nil', correct: true, question_id: q_ruby_data_types.id },
-                 { body: 'String, Integer, Boolean', correct: false, question_id: q_ruby_data_types.id },
-                 { body: 'Определение Класса', correct: true, question_id: q_ruby_class_define.id },
-                 { body: 'Ничего не делать', correct: false, question_id: q_ruby_class_define.id },
-                 { body: 'Воспользоваться фильтром', correct: true, question_id: q_jira_my_tasks.id },
-                 { body: 'Спросить у коллег', correct: false, question_id: q_jira_my_tasks.id },
-                 { body: 'Воспользоваться общим фильтром', correct: true, question_id: q_jira_team_tasks.id },
-                 { body: 'Спросить на стендапе', correct: false, question_id: q_jira_team_tasks.id },
-                 { body: 'Зайти в раздел deplyments', correct: true, question_id: q_kuber_pods_quantity.id },
-                 { body: 'Считать вручную', correct: false, question_id: q_kuber_pods_quantity.id },
-                 { body: 'Нажать кнопку exec', correct: true, question_id: q_kuber_pod_exec.id },
-                 { body: 'Нельзя заходить в Pod, это опасно', correct: false, question_id: q_kuber_pod_exec.id }
-               ])
+q_ruby_data_types.answers.create!([
+                                    { body: 'String, Integer, Nil', correct: true },
+                                    { body: 'String, Integer, Boolean', correct: false }
+                                  ])
+q_ruby_class_define.answers.create!([
+                                      { body: 'Определение Класса', correct: true },
+                                      { body: 'Ничего не делать', correct: false }
+                                    ])
+q_jira_my_tasks.answers.create!([
+                                  { body: 'Воспользоваться фильтром', correct: true },
+                                  { body: 'Спросить у коллег', correct: false }
+                                ])
+q_kuber_pods_quantity.answers.create!([
+                                        { body: 'Зайти в раздел deplyments', correct: true },
+                                        { body: 'Считать вручную', correct: false }
+                                      ])
+q_kuber_pod_exec.answers.create!([
+                                   { body: 'Нажать кнопку exec', correct: true },
+                                   { body: 'Нельзя заходить в Pod, это опасно', correct: false }
+                                 ])
 
 # Создание прохождений тестов
-TestPassing.create!([
-                      { passing_status: 'passed',      user_id: ivanov_user.id, test_id: ruby_test.id },
-                      { passing_status: 'in_progress', user_id: ivanov_user.id,  test_id: java_test.id },
-                      { passing_status: 'passed',      user_id: ivanov_user.id,  test_id: angular_test.id },
-                      { passing_status: 'in_progress', user_id: petrov_user.id,  test_id: bulma_test.id },
-                      { passing_status: 'passed',      user_id: petrov_user.id,  test_id: kubernetis_test.id },
-                      { passing_status: 'in_progress', user_id: petrov_user.id,  test_id: gitlab_test.id },
-                      { passing_status: 'passed',      user_id: sidorov_user.id, test_id: jira_test.id },
-                      { passing_status: 'in_progress', user_id: sidorov_user.id, test_id: ruby_test.id },
-                      { passing_status: 'passed',      user_id: sidorov_user.id, test_id: java_test.id }
-                    ])
+ivanov_user.test_passings.create!([
+                                    { passing_status: 'passed',      test_id: ruby_test.id },
+                                    { passing_status: 'in_progress', test_id: java_test.id },
+                                    { passing_status: 'passed',      test_id: angular_test.id }
+                                  ])
+petrov_user.test_passings.create!([
+                                    { passing_status: 'passed',      test_id: kubernetis_test.id },
+                                    { passing_status: 'in_progress', test_id: bulma_test.id },
+                                    { passing_status: 'in_progress', test_id: gitlab_test.id }
+                                  ])
+sidorov_user.test_passings.create!([
+                                     { passing_status: 'passed', test_id: jira_test.id },
+                                     { passing_status: 'in_progress', test_id: ruby_test.id },
+                                     { passing_status: 'passed',      test_id: java_test.id }
+                                   ])
