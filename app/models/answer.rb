0d72e37b209748606262d545c7faa6f9d81a8e09
Кшнякин Александр Владимирович <avkshniakin@domclick.ru>
2023-07-23 09:39:36 +0300

    fix validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,15 +4,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validate_answers_quantity, on: %i[create update]
+  validate :validate_answers_quantity, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_answers_quantity
-    return unless new_record? || (question.answers.pluck(:id).exclude? id)
-
     errors.add(:base) if question.answers.count > 3
   end
 end
