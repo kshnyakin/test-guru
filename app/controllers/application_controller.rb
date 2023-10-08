2cd9cc5aa97c42c17fee5c81209380785496d550
Кшнякин Александр Владимирович <avkshniakin@domclick.ru>
2023-10-08 10:54:47 +0300

    some works on screencasts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   def after_sign_in_path_for(resource)
     if current_user.admin?
@@ -9,6 +10,17 @@ class ApplicationController < ActionController::Base
     else
       super(resource)
     end
+  end
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
+  private
+
+  def set_locale
+    url_locale = params[:lang]
+    I18n.locale = I18n.locale_available?(url_locale) ? url_locale : I18n.default_locale
   end
 
   protected
